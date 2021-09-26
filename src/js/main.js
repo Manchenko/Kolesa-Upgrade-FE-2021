7c9b108d53724dd9a5e7e672b21e6fb4bc2a5e3a
MadinaShatayeva7 <91209026+MadinaShatayeva7@users.noreply.github.com>
2021-09-26 13:21:55 +0600

    Modal window1

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,21 @@
 import '../css/style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello, Madina!</h1>
-`;
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello, Madina Shatayeva!</h1>
+// `;
+
+// modal window
+
+// const btnOpen = document.getElementById('modal-open-btn');
+// const modal = document.getElementById('modal-wrap');
+// const overlay = document.getElementById('overlay');
+
+// btnOpen.addEventListener('click', () => {
+//     modal.classList.add('modal-wrap_active');
+// });
+
+// const closeModal = () => {
+//     modal.classList.remove('modal-wrap_active');
+// };
+
+// overlay.addEventListener('click', closeModal);

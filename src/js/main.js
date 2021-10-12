96fde9bfd536a88e6a5c526151452b8408a4d4b4
Ulzhan <kokenova.ulzhan@gmail.com>
2021-10-12 21:42:52 +0600

    add access and clothes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,60 @@
+const access = [{
+        id: 1,
+        title: "Рюкзак Синий",
+        price: 2500,
+        img: 'src/assets/bag.svg'
+    },
+    {
+        id: 2,
+        title: "Рюкзак Синий",
+        price: 2500,
+        img: 'src/assets/bag.svg'
+    },
+    {
+        id: 3,
+        title: "Рюкзак Синий",
+        price: 2500,
+        img: 'src/assets/bag.svg'
+    },
+    {
+        id: 4,
+        title: "Рюкзак Синий",
+        price: 2500,
+        img: 'src/assets/bag.svg'
+    },
+    {
+        id: 5,
+        title: "Рюкзак Синий",
+        price: 2500,
+        img: 'src/assets/bag.svg'
+    }
+]
+const cloth = [{
+        id: 6,
+        title: "Рюкзак Синий",
+        price: 2500,
+        img: 'src/assets/bag.svg'
+    },
+    {
+        id: 7,
+        title: "Рюкзак Синий",
+        price: 2500,
+        img: 'src/assets/bag.svg'
+    },
+    {
+        id: 8,
+        title: "Рюкзак Синий",
+        price: 2500,
+        img: 'src/assets/bag.svg'
+    },
+    {
+        id: 9,
+        title: "Рюкзак Синий",
+        price: 2500,
+        img: 'src/assets/bag.svg'
+    }
+
+]
 const modalClose = document.querySelector('.modal__close');
 const modal = document.querySelector('.hidden');
 const productCard = document.querySelector(".main-card");

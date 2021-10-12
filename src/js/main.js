8afe2c2f21fdc01dd4d4c96c722393b466ed04c4
Ulzhan <kokenova.ulzhan@gmail.com>
2021-10-13 00:00:07 +0600

    fix modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,34 @@
 const access = [{
         id: 1,
-        title: "Рюкзак Синий",
+        title: "Футболка Эволюцианируй или сдохни",
         price: 2500,
-        img: 'src/assets/bag.svg'
+        img: 'src/assets/image 4.png',
+        isNew: true
     },
     {
         id: 2,
-        title: "Рюкзак Синий",
+        title: "Футболка Эволюцианируй или сдохни",
         price: 2500,
-        img: 'src/assets/bag.svg'
+        img: 'src/assets/image 4.png',
+        isNew: true
     },
     {
         id: 3,
-        title: "Рюкзак Синий",
+        title: "Футболка Эволюцианируй или сдохни",
         price: 2500,
-        img: 'src/assets/bag.svg'
+        img: 'src/assets/image 4.png'
     },
     {
         id: 4,
-        title: "Рюкзак Синий",
+        title: "Футболка Эволюцианируй или сдохни",
         price: 2500,
-        img: 'src/assets/bag.svg'
+        img: 'src/assets/image 4.png'
     },
     {
         id: 5,
         title: "Рюкзак Синий",
         price: 2500,
-        img: 'src/assets/bag.svg'
+        img: 'src/assets/image 4.png'
     }
 ]
 const cloth = [{
@@ -68,3 +70,47 @@ document.querySelectorAll('.main-card').forEach(item => {
         modal.style.display = 'block';
     })
 })
+
+var makeElement = function(tagName, className, text) {
+    var listItem = document.createElement(tagName)
+    listItem.classList.add(className)
+    if (text) {
+        listItem.textContent = text
+    }
+    return listItem
+}
+
+
+var createElement = function(product) {
+    var listItem = makeElement('div', 'main-card')
+    var imgBlock = makeElement('div', 'main-image__block')
+    listItem.appendChild(imgBlock)
+    var img = makeElement('img', 'main-image')
+    img.src = product.img;
+    img.alt = product.title;
+    imgBlock.appendChild(img);
+    var badge = makeElement('span', 'badge')
+    badge.innerHTML = "new"
+    if (product.isNew) {
+        imgBlock.appendChild(badge);
+    }
+    var description = makeElement('div', 'main-image__description');
+    listItem.appendChild(description)
+    var price = makeElement('div', 'main-price', product.price);
+    description.appendChild(price);
+    var info = makeElement('h3', 'main-image__title', product.title)
+    description.appendChild(info)
+    var size = makeElement('div', 'main-image__hint')
+    size.innerHTML = "Размеры S/M/L"
+    description.appendChild(size)
+    var btn = makeElement('button', 'main-image__button')
+    btn.innerHTML = "Заказать"
+    description.appendChild(btn)
+
+    return listItem
+}
+var good = document.querySelector('.main-products')
+for (var i = 0; i < access.length; i++) {
+    var item = createElement(access[i])
+    good.append(item)
+}
